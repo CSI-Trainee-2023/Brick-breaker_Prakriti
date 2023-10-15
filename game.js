83533ffbe78be0da466af030a9b6df1ed53144ef
prkgr799 <prkgr799@gmail.com>
2023-10-15 13:20:43 +0530

    Adding ball and paddles

--- game.js
+++ game.js
@@ -2,12 +2,16 @@
 const cvs = document.getElementById("breakout");
 const ctx = cvs.getContext("2d");
 // Add border to canvas
-cvs.style.border = "1 px solid #off";
-
+cvs.style.border = "1 px solid #0ff";
+ctx.lineWidth = 3;
 // Game variables and constants
 const PADDLE_WIDTH = 100;
 const PADDLE_MARGIN_BOTTOM = 50;
 const PADDLE_HEIGHT = 20;
+const BALL_RADIUS = 8;
+let LIFE = 3; 
+let leftArrow = false;
+let rightArrow = false;
 
 // Create the Paddle
 const paddle = {
@@ -23,5 +27,118 @@ function drawPaddle(){
     ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
 
     ctx.strokeStyle = "#ffcd05";
-    ctx.StrokeRect(paddle.x, paddle.y, paddle.width, paddle.height);
+    ctx.StrokeRect(paddle.x, paddle.y, paddle.width, paddle.height);  
 }
+
+//Control the paddle
+document.addEventListener("keydown", function(event)
+{
+    if(event.DOM_KEY_LOCATION_LEFT== 37){
+        leftArrow = true;
+    } else if(event.DOM_KEY_LOCATION_RIGHT== 39){
+        rightArrow= true;
+    }
+}); 
+document.addEventListener("keyup", function(event)
+{
+    if(event.DOM_KEY_LOCATION_LEFT== 37){
+        leftArrow = true;
+    } else if(event.DOM_KEY_LOCATION_RIGHT== 39){
+        rightArrow= true;
+    }
+}); 
+
+//Move paddle
+function movePaddle(){
+    if(rightArrow && paddle.x + paddle.width < cvs.width){
+        paddle.x += paddle.dx;
+    }else if(leftArrow && paddle.x > 0){
+        paddle.x -= paddle.dx;
+    }
+}
+//Create the ball
+const ball ={
+    x: cvs.width/2,
+    y:paddle.y - BALL_RADIUS,
+    radius : BALL_RADIUS,
+    speed : 4,
+    dx : 3 * (Math.random()*2-1),
+    dy : -3,
+}
+
+//Draw the ball
+function drawBall(){
+    ctx.beginPath();
+
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI*2);
+    ctx.fillStyle = "#ffcd05";
+    ctx.fill();
+
+    ctx.strokeStyle = "2e3548";
+    ctx.stroke();
+    ctx.closePath();
+}
+//Move the ball
+function moveBall(){
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+}
+
+// Ball and wall collision detection
+function ballwallCollision(){
+   if(ball.x + ball.radius > cvs.width || ball.x - ball.radius < 0){
+     ball.dx = -ball.dx;
+   } 
+   if(ball.y - ball.radius < 0){
+    ball.dy = -ball.dy;
+   }
+   if(ball.y + ball.radius > cvs.height){
+    LIFE--;  //Lose life
+    resetBall();
+   }
+}
+//Reset the ball
+function resetBall(){
+    ball.x= cvs.width/2;
+    ball.y= paddle.y = BALL_RADIUS;
+    ball.dx= 3 * (Math.random()*2-1);
+    ball.dy= -3;
+
+}
+
+//Ball and paddle collision
+function ballPaddleCollision(){
+    if(ball.x < paddle.x + paddle.width && ball.x > paddle.x && paddle.y < paddle.y + paddle.height && ball.y > paddle.y){
+
+        let  collidePoint = ball.x - (paddle.x + paddle.width/2);
+        collidePoint = collidePoint / (paddle.width/2);
+        let angle = collidePoint * Math.PI/3;
+        ball.dx = ball.speed * Math.sin(angle);
+        ball.dy = ball.speed * Math.cos(angle);
+    }
+}
+
+
+// draw function
+function draw(){
+    drawPaddle();
+    drawBall();
+}
+
+
+// update game function
+function update(){
+    movePaddle();
+    moveBall();
+    ballwallCollision();
+    ballPaddleCollision();
+}
+
+function loop(){
+    ctx.drawImage(BG_IMG, 0, 0);
+     draw();
+     update();
+     requestAnimationFrame(loop);
+
+}
+loop();
